297b320bad4f7e05023d44354fcb86d3092cddb8
hs12 <loveanna1228@gmail.com>
2023-11-27 18:49:12 +0900

    [Refactor] 유저통합테스트 저장소에 의존하지 않도록 수정

--- src/test/java/com/kdt/simpleboard/user/controller/UserControllerTest.java
+++ src/test/java/com/kdt/simpleboard/user/controller/UserControllerTest.java
@@ -2,16 +2,14 @@ package com.kdt.simpleboard.user.controller;
 
 import com.kdt.simpleboard.BaseIntegrationTest;
 import com.kdt.simpleboard.user.UserData;
-import com.kdt.simpleboard.user.domain.User;
-import com.kdt.simpleboard.user.dto.UserRequest;
-import com.kdt.simpleboard.user.repository.UserRepository;
-import org.junit.jupiter.api.*;
+import com.kdt.simpleboard.user.service.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
-import static com.kdt.simpleboard.user.dto.UserRequest.*;
-import static org.springframework.http.MediaType.*;
+import static com.kdt.simpleboard.user.dto.UserRequest.CreateUserRequest;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,12 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 class UserControllerTest extends BaseIntegrationTest {
     @Autowired
-    private UserRepository userRepository;
-
-    @BeforeEach
-    void clear() {
-        userRepository.deleteAll();
-    }
+    private UserService userService;
 
     @Test
     @DisplayName("회원 생성 api 호출에 성공한다.")
@@ -41,13 +34,12 @@ class UserControllerTest extends BaseIntegrationTest {
     @Test
     @DisplayName("중복 이름의 회원이 존재하면 회원 api 생성 호출에 실패한다.")
     void createUserWithFail() throws Exception {
-        User user = UserData.user();
-        userRepository.save(user);
-        CreateUserRequest userReq = UserData.createUserReq();
+        CreateUserRequest createUserRequest = UserData.createUserReq();
+        userService.createUser(createUserRequest);
 
         mvc.perform(post("/users")
                         .contentType(APPLICATION_JSON)
-                        .content(asJsonString(userReq))
+                        .content(asJsonString(createUserRequest))
                         .accept(APPLICATION_JSON))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.message").value("해당 이름의 회원이 이미 존재합니다."))
